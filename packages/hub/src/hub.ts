6af3933372965e84fe24a877578f54a93a49ccd5
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-08-20 14:16:23 +0200

    fix: Span changes

--- packages/hub/src/hub.ts
+++ packages/hub/src/hub.ts
@@ -396,7 +396,7 @@ export class Hub implements HubInterface {
       const span = top.scope.getSpan();
 
       if (span) {
-        return span.newSpan(spanContext);
+        return span.makeChild(spanContext);
       }
     }
 
